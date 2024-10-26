9de43da49de09c1d1c524e6f837f2288bee2b0e3
ZhangBoning21 <116284242+ZhangBoning21@users.noreply.github.com>
2024-10-26 21:38:40 +0800

    Add files via upload

---
+++ scripts/HandBody-fusion.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# 要删除的文件的路径
+file_to_delete1="/data/boning_zhang/blend_body_hand_pipline/4dhuman_out.pkl"
+file_to_delete2="/data/boning_zhang/blend_body_hand_pipline/hamer_out.pkl"
+checkpoint_human4d="/data/boning_zhang/4D-Humans/logs/freeman_score_hamer/runs/finetune_on_hardmoplus/checkpoints/epoch=9-step=50000.ckpt"
+# 删除第一个文件
+if [ -f "$file_to_delete1" ]; then
+    rm "$file_to_delete1"
+    echo "$file_to_delete1 has been deleted."
+else
+    echo "$file_to_delete1 does not exist."
+fi
+
+# 删除第二个文件
+if [ -f "$file_to_delete2" ]; then
+    rm "$file_to_delete2"
+    echo "$file_to_delete2 has been deleted."
+else
+    echo "$file_to_delete2 does not exist."
+fi
+# Activate the first conda environment
+source activate 4D_humans_copy_1.6
+
+# Run the first Python script
+cd /data/boning_zhang/4D-Humans/
+CUDA_VISIBLE_DEVICES=7 python demo_onlyone.py --img_folder /data/boning_zhang/blend_body_hand_pipline/example_vis --batch_size=48 --save_smpl --checkpoint "$checkpoint_human4d"
+
+conda deactivate #这个是必要的，要不然出错
+# Activate the second conda environment
+source activate hamer
+
+# Run the second Python script
+cd /data/boning_zhang/hamer/
+CUDA_VISIBLE_DEVICES=7 python demo_onlyone.py --img_folder /data/boning_zhang/blend_body_hand_pipline/example_vis  --batch_size=48 --save_smpl
+
+#python hand_body_demo.py --img_folder /data/boning_zhang/blend_body_hand_pipline/example_data --out_folder /data/boning_zhang/blend_body_hand_pipline/demo_out --batch_size=48  --save_render --full_frame
+
+conda deactivate #这个是必要的，要不然出错
+# Activate the second conda environment
+source activate pymaf-x
+cd /data/boning_zhang/PyMAF-X
+CUDA_VISIBLE_DEVICES=7 python -m apps.render_arms --image_folder /data/boning_zhang/blend_body_hand_pipline/example_vis --detection_threshold 0.3 --pretrained_model data/pretrained_model/PyMAF-X_model_checkpoint_v1.1.pt --misc TRAIN.BHF_MODE full_body MODEL.PyMAF.HAND_VIS_TH 0.1 --no_render --output_folder /data/boning_zhang/blend_body_hand_pipline
+
+conda deactivate #这个是必要的，要不然出错
+# Activate the second conda environment
+source activate 4D_humans_copy_1.6
+
+cd /data/boning_zhang/4D-Humans
+CUDA_VISIBLE_DEVICES=7 python mydemo1.py --checkpoint "$checkpoint_human4d"
+
+#python demo.py --img_folder /data/boning_zhang/blend_body_hand_pipline/example_data --out_folder /data/boning_zhang/blend_body_hand_pipline/4dhuman_demo_out --batch_size=48 --side_view --save_mesh --full_frame
